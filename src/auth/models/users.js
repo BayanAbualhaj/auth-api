df95320ebd54ebea7005299a2c09eca13686f164
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2021-04-06 21:01:26 +0300

    shuuuuuuuu

--- src/auth/models/users.js
+++ src/auth/models/users.js
@@ -3,6 +3,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const dotenv=require('dotenv');
+dotenv.config()
 
 const users = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
